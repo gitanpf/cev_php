e39e4eeeed737d5efbc5fa7e19f8d77dbd2c954b
contaparaestudo-uwu <contato.anpf@gmail.com>
2026-05-21 13:28:22 -0300

    Adicionando exercício 3 aula 21

--- ex003/index.php
+++ ex003/index.php
@@ -4,12 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tipos primitivos em PHP</title>
 </head>
 
 <body>
+    <h1>Testes de tipos primitivos</h1>
     <?php 
-        
+        // 0x = hexadecimal | 0b = binário | 0 = octal
+        //$num = 010;
+        //echo "O valor da variável é $num";
+
+        //Saber tipo primitivo da var:
+        //$v = 300;
+        //var_dump($v);
+
+        //$num = 3e2; //3 x 10² =300 coerção
+        //echo "O valor é $num";
+
+        //coerção
+        //$num = (int) "950"; //diz o tipo nos ()
+        //var_dump($num);
+
+        //$num = true; //true vale 1 e false é vazio
+        //var_dump($num);
     ?>
 </body>
 
